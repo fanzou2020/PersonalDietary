0bf8b091a95c390a8ebabf17fc9d67287543a328
Xin Ma <xma@momentis.com>
2020-01-27 09:43:12 -0500

    add structure

---
+++ src/team4/personaldietary/application/PersonalDietaryApp.java
@@ -0,0 +1,7 @@
+package team4.personaldietary.application;
+
+public class PersonalDietaryApp {
+    public static void main(String[] args) {
+        System.out.println("team4.personalDietary.application.Hello!");
+    }
+}
