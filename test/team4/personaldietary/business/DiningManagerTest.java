9270bf60db373bda5ae8cc062b3a8b186ce88482
Oz <frost57@gmail.com>
2020-03-12 14:50:22 -0400

    Added some JUnit tests for increment 2

--- test/team4/personaldietary/business/DiningManagerTest.java
+++ test/team4/personaldietary/business/DiningManagerTest.java
@@ -2,56 +2,94 @@ package team4.personaldietary.business;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import org.junit.jupiter.api.BeforeEach;
+import javafx.scene.control.ListView;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import team4.personaldietary.GUI.DiningTableRow;
-import team4.personaldietary.bean.Dining;
-import team4.personaldietary.bean.Indining;
-import team4.personaldietary.bean.Outdining;
+import team4.personaldietary.bean.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 class DiningManagerTest {
 
-    ArrayList<Dining> expected = new ArrayList<>();
-    private ObservableList<String> currServingList;
-    private ObservableList<String> consumedServingList;
+    private ArrayList<Dining> diningArrayList; // consider this as the data model.
+    private ArrayList<DiningTableRow> diningTableRowArrayList; // this contains all the TableRow items
+    private Collection<DiningTableRow> observableCollection; // this is the items shown on the screen
+    private ObservableList<String> currServingList, consumedServingList; // ListView for serving
 
-    private ObservableList<DiningTableRow> diningList;
+    private ArrayList<Dining> testDiningArrayList = new ArrayList<Dining>();
+    private ArrayList<DiningTableRow> testDiningTableRowArrayList = new ArrayList<DiningTableRow>();
+    private Collection<DiningTableRow> testObservableCollection = FXCollections.observableArrayList();
+    private ObservableList<String> testCurrServingList = FXCollections.observableArrayList(), testConsumedServingList = FXCollections.observableArrayList();
 
-    private DiningManager diningManager;
-
-
-    Dining diningItem1;
-    Dining diningItem2;
-    Dining diningItem3;
-
-    @BeforeEach
-    void setUp() {
-        diningItem1 = new Indining();
-        diningItem2 = new Outdining();
-        diningItem3 = new Indining();
-        diningList = FXCollections.observableArrayList();
-        consumedServingList = FXCollections.observableArrayList();
-        currServingList = FXCollections.observableArrayList();
-        diningManager = new DiningManager(diningList, currServingList, consumedServingList);
+    @Test
+    void DiningManager() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void getDiningArrayList() {
+
+        DiningManager manager = new DiningManager(testObservableCollection, testCurrServingList, testConsumedServingList);
+        assertEquals(manager.getDiningArrayList().size(), 0, "testing getDinaryArrayList()");
+    }
+
+    @Test
     void addDiningItem() {
 
-        diningManager.addDiningItem(diningItem1);
-        diningManager.addDiningItem(diningItem2);
-        diningManager.addDiningItem(diningItem3);
+        DiningManager manager = new DiningManager(testObservableCollection, testCurrServingList, testConsumedServingList);
 
-        assertEquals(3, diningManager.getDiningArrayList().size());
+        LocalDateTime time = LocalDateTime.now();
+        FoodGroup group = FoodGroup.grain_products;
+        Serving serving = new Serving("a", 1.0, 1.0, 1.0, 1.0);
+        Dining a = new Indining("1", time, group, serving, "1", "1");
+        Dining b = new Outdining("1", time, group, serving, "1", "1");
+
+        manager.addDiningItem(null);
+        manager.addDiningItem(a);
+
+        assertEquals(manager.getDiningArrayList().get(0), a, "testing addDiningItem()");
+        assertEquals(manager.getDiningArrayList().get(1), b, "testing addDiningItem()");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void removeDiningItem() {
-        diningManager.addDiningItem(diningItem1);
-        diningManager.removeDiningItem(diningItem1);
 
-        assertEquals(0, diningManager.getDiningArrayList().size());
+        LocalDateTime time = LocalDateTime.now();
+        FoodGroup group = FoodGroup.grain_products;
+        Serving serving = new Serving("a", 1.0, 1.0, 1.0, 1.0);
+        Dining a = new Indining("1", time, group, serving, "1", "1");
+        Dining b = new Outdining("1", time, group, serving, "1", "1");
+
+        testDiningArrayList.add(a);
+        testDiningArrayList.add(b);
+
+        //.re
+
+    }
+
+    @Test
+    void markConsumed() {
+    }
+
+    @Test
+    void markUnConsumed() {
+    }
+
+    @Test
+    void hideConsumed() {
+    }
+
+    @Test
+    void unHideConsumed() {
+    }
+
+    @Test
+    void updateCurrServing() {
+    }
+
+    @Test
+    void updateConsumedServing() {
     }
 }
